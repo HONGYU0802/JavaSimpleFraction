55677ac2b66680580cbbcb4ef041a645284755b6
younesbens <younes.benyamna@gmail.com>
2017-10-03 20:30:59 +0200

    modifs

--- td1/exo4.1/ChaineCryptee.java
+++ td1/exo4.1/ChaineCryptee.java
@@ -1,14 +1,13 @@
 import java.*;
 /**
- * Décrivez votre classe ChaineCryptee ici.
- *
- * @author (votre nom)
- * @version (un numéro de version ou une date)
+ * RAS
+ * @author Benyamna younes
+ * @version 4.1
  */
 public class ChaineCryptee
 {
     // variables d'instance - remplacez l'exemple qui suit par le vôtre
-    private String chaineClair;
+    private String chaineCryptee;
     private int decalage;
 
     /**
@@ -17,9 +16,15 @@ public class ChaineCryptee
      */
     private ChaineCryptee(String ChaineClair, int Decalage)
     {
-        decalage = Decalage;
-        chaineClair = ChaineClair;
         if (chaineClair == null) { chaineClair = "A"; }
+        decalage = Decalage;
+        String ch = "";
+        int i;
+        char c;
+        for(i=0;i<chaineClair.length();i++)
+           { c=decaleCaractere(chaineClair.charAt(i),decalage); 
+             ch += c;}
+        chaineCryptee = ch;
     }
 
     /**
@@ -28,23 +33,40 @@ public class ChaineCryptee
      * @param  y   le paramètre de la méthode
      * @return     la somme de x et de y
      */
-    public String Decrypte()
+    public String crypte()
     {
-        return chaineClair;
+        return chaineCryptee;
     }
     
     public static char decaleCaractere ( char c , int decalage ) {
      return (c < 'A' || c > 'Z' ) ? c : (char) ((( c - 'A' + decalage)%26 ) + 'A');
     }
 
-    public String Crypte()
+    public String Decrypte()
     {
         String ch = "";
         int i;
         char c;
-        for(i=0;i<chaineClair.length();i++)
-           { c=decaleCaractere(chaineClair.charAt(i),this.decalage); 
+        for(i=0;i<chaineCryptee.length();i++)
+           { c=decaleCaractere(chaineCryptee.charAt(i), - decalage); 
              ch += c;}
+        return ch;
+    }
+    
+        public static ChaineCryptee FromCrypted(String crypte, int decalage) { 
+
+        int i;
+        String ch = "";
+        char c;
+        for(i=0;i<crypte.length();i++)
+           { c=decaleCaractere(crypte.charAt(i), - decalage ); 
+             ch += c;}
+        ChaineCryptee ch2 = new ChaineCryptee(ch, decalage);
+        return ch2;
+    }
+    
+    public static ChaineCryptee FromDecrypted(String enclair, int decalage) {
+        ChaineCryptee ch = new ChaineCryptee(enclair, decalage);
         return ch;
     }
 }
